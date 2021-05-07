f7dfbb53a20a05f92ffe1c55f51ae972bf78fc59
Manuel <mninahuanca4@gmail.com>
2021-05-07 11:27:05 -0400

    Css a la navegacion

--- src/components/navegacion.js
+++ src/components/navegacion.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+    display:flex;
+    flex-direction:column;
+    padding-bottom:3rem;
+    @media(min-width:768px){
+        padding:0;
+        flex-direction:row;
+    }
+`;
+
+const NavLink = styled(Link)`
+    color:#fff;
+
+`;
 
 const Navegacion = () => {
     return (
-        <nav>
-            <Link to='/'>Inicio</Link>
-            <Link to='nosotros'>Nosotros</Link>
-            <Link to='/propiedades'>Propiedades</Link>
-        </nav>
+        <Nav>
+            <NavLink to='/'>Inicio</NavLink>
+            <NavLink to='nosotros'>Nosotros</NavLink>
+            <NavLink to='/propiedades'>Propiedades</NavLink>
+        </Nav>
      );
 }
 
