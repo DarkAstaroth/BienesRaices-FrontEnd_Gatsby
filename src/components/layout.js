90dc746db8ff4fc11baa607a2b896be3425ac78f
Manuel <mninahuanca4@gmail.com>
2021-05-07 10:20:47 -0400

    Instalando Styled Components en Gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/react';
 
 const Layout = ({children}) => {
     return (
         <>
+            <Global
+                styles={css`
+                    body{
+                        background-color: red;
+                    }
+                `}
+            />
+
             <Helmet>
                 <title>Bienes Raices Gatsby</title>
                 <meta name="description" content="Sitio web de bienes raices en Gatsby"/>
