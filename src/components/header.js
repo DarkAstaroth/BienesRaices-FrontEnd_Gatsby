28c021c31b887a4325c332e2081cde4b9fc1ab4c
Manuel <mninahuanca4@gmail.com>
2021-05-07 10:56:11 -0400

    CSS al header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Navegacion from './navegacion';;
+import Navegacion from './navegacion';
+import { css } from '@emotion/react';
 
 const Header = () => {
     return (
-        <header>
-            <div>
+        <header
+            css={css`
+                background-color:#0d283b;
+                padding:1rem;
+
+            `}
+        >
+            <div
+                css={css`
+                    max-width:120rem;
+                    margin:0 auto;
+                    text-align:center;
+
+                    @media(min-width:768px){
+                        display:flex;
+                        align-items:center;
+                        justify-content:space-between;
+                    }
+                `}
+            >
                 <Link>
                     Bienes Raices
                 </Link>
