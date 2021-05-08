ab169e30c37120f4bff9aaddee1d3c32986c2d51
Manuel <mninahuanca4@gmail.com>
2021-05-08 10:55:54 -0400

    consultando para obtener todas las propiedades

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import styled from '@emotion/styled';
 import BackgroundImage from 'gatsby-background-image';
 import * as heroCSS from '../css/hero.module.css';
 import Encuentra from '../components/encuentra';
+import ListadoPropiedades from '../components/listadoPropiedades';
 
 const ImagenBackground = styled(BackgroundImage)`
      height : 600px;
@@ -44,7 +45,8 @@ const Index = () => {
                     
                 </div>
             </main>
-            <Encuentra/>
+            <Encuentra />
+            <ListadoPropiedades />
         </Layout>
     );
 }
