347a0785926e45a133033327d5a16a2ff0cacbde
Manuel <mninahuanca4@gmail.com>
2021-05-08 10:24:00 -0400

    CSS a la imagen

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import useInicio from '../hooks/useInicio';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import BackgroundImage from 'gatsby-background-image';
+import * as heroCSS from '../css/hero.module.css';
 
 const ImagenBackground = styled(BackgroundImage)`
      height : 600px;
@@ -22,8 +23,8 @@ const Index = () => {
                 fluid={imagen.sharp.fluid}
                 fadeIn="soft"
             >
-                <div>
-                    <h1>Venta de casa y departamentos exclusivos</h1>
+                <div className={heroCSS.imagenbg}>
+                    <h1 className={heroCSS.titulo}>Venta de casa y departamentos exclusivos</h1>
                 </div>
             </ImagenBackground>
             <main>
