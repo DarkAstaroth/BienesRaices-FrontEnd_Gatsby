525681d9b24807c98bc9368cb04d6ed4677a7bef
Manuel <mninahuanca4@gmail.com>
2021-05-09 20:37:07 -0400

    Mostrando la informacion de la base de datos

--- src/components/propiedades.js
+++ src/components/propiedades.js
@@ -38,7 +38,27 @@ const Propiedad = ({ data: { allStrapiPropiedades: { nodes } } }) => {
 
     return (
         <Layout>
-            <h1>{ nombre }</h1>
+            <h1>{nombre}</h1>
+            <div>
+                <main>
+                    <Img fluid={imagen.sharp.fluid} />
+                    <p>{description}</p>
+                </main>
+                <aside>
+                    <p>$ {precio}</p>
+                    <Iconos
+                        wc={wc}
+                        estacionamiento={estacionamiento}
+                        habitaciones={habitaciones}
+                    />
+                    <div>
+                        <h2>Vendedor:</h2>
+                        <p>{agentes.nombre}</p>
+                        <p>tel: {agentes.telefono}</p>
+                        <p>Email:{ agentes.email}</p>
+                    </div>
+                </aside>
+            </div>
         </Layout>
     );
 }
