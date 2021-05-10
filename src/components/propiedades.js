0587f13af988d76d18bf0b8f757c079b4cddb1b3
Manuel <mninahuanca4@gmail.com>
2021-05-09 20:32:28 -0400

    Obteniendo el contenido de graphql

--- src/components/propiedades.js
+++ src/components/propiedades.js
@@ -10,19 +10,37 @@ export const query = graphql`
         allStrapiPropiedades(filter:{id:{eq:$id}}){
             nodes{
                 nombre
+                description
+                wc
                 estacionamiento
+                habitaciones
+                precio
+                agentes {
+                    nombre
+                    telefono
+                    email
+                }
+                imagen {
+                    sharp : childImageSharp {
+                        fluid (maxWidth : 1200){
+                            ...GatsbyImageSharpFluid_withWebp
+                        }
+                    }
+                }
             }
         }
     }
 `;
 
-const Propiedad = ({data}) => {
+const Propiedad = ({ data: { allStrapiPropiedades: { nodes } } }) => {
+
+    const { nombre, description, wc, estacionamiento, habitaciones, agentes, imagen, precio } = nodes[0];
 
     return (
         <Layout>
-
+            <h1>{ nombre }</h1>
         </Layout>
-     );
+    );
 }
- 
+
 export default Propiedad;
